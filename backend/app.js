2e901a341f3a6ddcd480a0d721a0ff21a04a28f5
Zakir Matloob <145646219+ZakirCodeArchitect@users.noreply.github.com>
2024-08-11 15:06:33 +0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT
 app.use(express.json())
 app.use(cors(
     {
-        origin: ["https://expense-tracker-mern-frontend.vercel.app/"],
+        origin: ["https://expense-tracker-mern-frontend.vercel.app"],
         methods: ["POST","GET","DELETE"],
         credentials: true
     }
