5abb1807288ec4f9554dfd721d24ced2798df608
Muhammad Adnan Sultan <114609178+madnansultandotme@users.noreply.github.com>
2024-08-25 19:47:35 +0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose'); // Import mongoose
 const cors = require('cors')
 const { db } = require('./db/db')
 const {readdirSync} = require('fs')
